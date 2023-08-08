11347b841b4fca470c0f2463fd9dcc2827214c2e
zuoshuai <zuoshuai@mininglamp.com>
2023-08-08 09:59:10 +0800

    Add register callback for vue example

--- example-vue/src/HowxmExample.vue
+++ example-vue/src/HowxmExample.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script>
-import { initHowxm, identifyHowxm, openHowxm, checkOpenHowxm, eventHowxm, setExtraAttributes } from 'howxm-js'
+import { initHowxm, identifyHowxm, openHowxm, checkOpenHowxm, eventHowxm, setExtraAttributes, onBeforeOpenHowxm, onOpenHowxm, onCloseHowxm, onPageCompleteHowxm, onCompleteHowxm } from 'howxm-js'
 const myLogger = console.info
 
 const campaignId = '<Your published campaignId>'
@@ -22,6 +22,26 @@ export default {
     const isReady = initHowxm(appId, myLogger)
     if (isReady) {
       identifyHowxm({ uid })
+
+      onBeforeOpenHowxm((campaignId, uid, attributes) => {
+        myLogger('onBeforeOpenHowxm: ', campaignId, uid, attributes)
+      })
+
+      onOpenHowxm((campaignId, uid, attributes) => {
+        myLogger('onOpenHowxm: ', campaignId, uid, attributes)
+      })
+
+      onCloseHowxm((campaignId, uid) => {
+        myLogger('onCloseHowxm: ', campaignId, uid)
+      })
+
+      onPageCompleteHowxm((campaignId, uid, fieldsEntry) => {
+        myLogger('onPageCompleteHowxm: ', campaignId, uid, fieldsEntry)
+      })
+
+      onCompleteHowxm((campaignId, uid) => {
+        myLogger('onCompleteHowxm: ', campaignId, uid)
+      })
     }
   },
   methods: {
